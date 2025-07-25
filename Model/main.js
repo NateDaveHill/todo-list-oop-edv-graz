a99d24a7c1d5438d633f8b46f0fd56012c7aa7c3
ndh <natedavehill@gmail.com>
2025-07-25 14:00:55 +0200

    fixed code ai for help

--- Model/main.js
+++ Model/main.js
@@ -4,15 +4,19 @@ import Todo from "./Todo.js";
 import { saveToLocalStorage,loadFromLocalStorage } from "../Controller/local-storage.js";
 import { renderProjects } from "../View/render.js";
 
+// Load tempProjectId from localStorage
 let tempProjectId = 0;
-
-let projects = [];
-const projectsFromLocalStorage = loadFromLocalStorage('projects')
-if(projectsFromLocalStorage) {
-    projects = projectsFromLocalStorage
-    renderProjects(projects);
+const tempIdFromStorage = localStorage.getItem('tempProjectId');
+if (tempIdFromStorage) {
+    tempProjectId = parseInt(tempIdFromStorage);
 }
 
+let projects = [];
+const projectsFromLocalStorage = loadFromLocalStorage('projects');
+if (projectsFromLocalStorage) {
+    projects = projectsFromLocalStorage;
+    renderProjects(projects);
+}
 
 const addProjectBtn = document.querySelector('.add-proj-btn');
 let inputName = document.querySelector('#proj_name');
@@ -22,26 +26,28 @@ addProjectBtn.addEventListener('click', () => {
     saveToLocalStorage(projects, "projects");
     renderProjects(projects);
     tempProjectId = project.id;
-})
-
+    localStorage.setItem('tempProjectId', tempProjectId);
+});
 
 const addTodoBtn = document.querySelector('.add-todo-btn');
 let inputTitle = document.querySelector('#todo_title');
 let inputDesc = document.querySelector('#todo_desc');
 let inputPrio = document.querySelector('#todo_prio');
-addTodoBtn.addEventListener('click', () => {
+
+addTodoBtn.addEventListener('click', (e) => {
+    e.preventDefault();
     const todo = new Todo(Date.now(), inputTitle.value, inputDesc.value, inputPrio.value);
-    if(projectsFromLocalStorage) {
-        if (tempProjectId !== 0) {
-            const project = projects.find(p => p.id === tempProjectId);
-            if (project) {
-                project.todos.push(todo);
-                saveToLocalStorage(projects, "projects");
-                renderProjects(projects);
-                console.log("Todo added:", todo);
-            } else {
-                console.warn("Project not found for ID:", tempProjectId);
-            }
+    if (tempProjectId !== 0) {
+        const project = projects.find(p => p.id === tempProjectId);
+        if (project) {
+            project.todos.push(todo);
+            saveToLocalStorage(projects, "projects");
+            renderProjects(projects);
+            console.log("Todo added:", todo);
+        } else {
+            console.warn("Project not found for ID:", tempProjectId);
         }
+    } else {
+        console.warn("No tempProjectId set");
     }
-})
+});
